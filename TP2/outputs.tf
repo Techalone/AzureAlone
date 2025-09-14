b6423c8480945e2b47b60585b7b8af86f3dbc19e
Alone VAITANAKI <alone.vaitanaki@efrei.net>
2025-09-14 18:58:03 +0200

    modif

--- TP2/outputs.tf
+++ TP2/outputs.tf
@@ -1,11 +1,9 @@
-# outputs.tf
-
 output "public_ip" {
-  description = "L'adresse IP publique de la VM"
+  description = "Adresse IP publique de la VM"
   value       = azurerm_public_ip.main.ip_address
 }
 
 output "public_dns" {
-  description = "Le nom DNS complet de la VM"
+  description = "Nom DNS complet de la VM"
   value       = azurerm_public_ip.main.fqdn
 }
